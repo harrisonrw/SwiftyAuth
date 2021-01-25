e0ac12b7139e3c4a1cc83250eed10a0959e63ad8
Robert Harrison <harrisonrw@gmail.com>
2021-01-25 11:13:37 -0800

    Initial Xcode Project

---
+++ SwiftyAuth/SwiftyAuth/SwiftyAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyAuthApp.swift
+//  SwiftyAuth
+//
+//  Created by Robert Harrison on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
