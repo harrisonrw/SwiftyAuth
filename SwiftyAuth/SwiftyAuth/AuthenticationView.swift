f55d79bdd0a4e2a086796c575543c8ee7db3e644
Robert Harrison <harrisonrw@gmail.com>
2021-08-09 10:15:20 -0700

    Add Navigation View with a Close Button

--- SwiftyAuth/SwiftyAuth/AuthenticationView.swift
+++ SwiftyAuth/SwiftyAuth/AuthenticationView.swift
@@ -20,106 +20,120 @@ struct AuthenticationView: View {
     
     var body: some View {
         
-        ZStack {
+        NavigationView {
             
-            Theme.Color.background
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
+            ZStack {
                 
-                Image("swift-logo-orange")
-                    .padding()
+                Theme.Color.background
+                    .edgesIgnoringSafeArea(.all)
                 
-                if isRegisterPresented {
+                VStack {
                     
-                    TextField("First Name", text: $firstName)
-                        .frame(height: 54)
-                        .textFieldStyle(.plain)
-                        .textContentType(.givenName)
-                        .padding(.horizontal, 10)
-                        .cornerRadius(8)
-                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 4)
-                    
-                    TextField("Last Name", text: $lastName)
-                        .frame(height: 54)
-                        .textFieldStyle(.plain)
-                        .textContentType(.familyName)
-                        .padding(.horizontal, 10)
-                        .cornerRadius(8)
-                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 4)
-                    
-                }
-                
-                TextField("Email", text: $email)
-                    .frame(height: 54)
-                    .textFieldStyle(.plain)
-                    .textContentType(.username)
-                    .padding(.horizontal, 10)
-                    .cornerRadius(8)
-                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 4)
-                
-                
-                SecureField("Password", text: $password)
-                    .frame(height: 54)
-                    .textFieldStyle(.plain)
-                    .textContentType(.password)
-                    .padding(.horizontal, 10)
-                    .cornerRadius(8)
-                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 4)
-                
-                Button {
+                    Image("swift-logo-orange")
+                        .padding()
                     
                     if isRegisterPresented {
-                        // Register
-                    } else {
-                        // Sign In
+                        
+                        TextField("First Name", text: $firstName)
+                            .frame(height: 54)
+                            .textFieldStyle(.plain)
+                            .textContentType(.givenName)
+                            .padding(.horizontal, 10)
+                            .cornerRadius(8)
+                            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 4)
+                        
+                        TextField("Last Name", text: $lastName)
+                            .frame(height: 54)
+                            .textFieldStyle(.plain)
+                            .textContentType(.familyName)
+                            .padding(.horizontal, 10)
+                            .cornerRadius(8)
+                            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 4)
+                        
                     }
                     
-                    presentationMode.wrappedValue.dismiss()
-                    
-                } label: {
-                    let actionButtonTitle = isRegisterPresented ? "Register" : "Sign In"
-                    Text(actionButtonTitle)
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .padding()
-                        .foregroundColor(Theme.Color.buttonText)
-                        .background(Theme.Color.buttonBackground)
-                        .cornerRadius(Theme.cornerRadius)
-                        .font(.system(size: 18.0, weight: .semibold))
-                        .padding(.horizontal, 20.0)
+                    TextField("Email", text: $email)
+                        .frame(height: 54)
+                        .textFieldStyle(.plain)
+                        .textContentType(.username)
+                        .padding(.horizontal, 10)
+                        .cornerRadius(8)
+                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
+                        .padding(.horizontal, 20)
                         .padding(.vertical, 4)
-                }
-                
-                if !isRegisterPresented {
-                    Button("Reset Password") {
-                        print("Reset Password")
+                    
+                    
+                    SecureField("Password", text: $password)
+                        .frame(height: 54)
+                        .textFieldStyle(.plain)
+                        .textContentType(.password)
+                        .padding(.horizontal, 10)
+                        .cornerRadius(8)
+                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 4)
+                    
+                    Button {
+                        
+                        if isRegisterPresented {
+                            // Register
+                        } else {
+                            // Sign In
+                        }
+                        
+                        presentationMode.wrappedValue.dismiss()
+                        
+                    } label: {
+                        let actionButtonTitle = isRegisterPresented ? "Register" : "Sign In"
+                        Text(actionButtonTitle)
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .padding()
+                            .foregroundColor(Theme.Color.buttonText)
+                            .background(Theme.Color.buttonBackground)
+                            .cornerRadius(Theme.cornerRadius)
+                            .font(.system(size: 18.0, weight: .semibold))
+                            .padding(.horizontal, 20.0)
+                            .padding(.vertical, 4)
+                    }
+                    
+                    if !isRegisterPresented {
+                        Button("Reset Password") {
+                            print("Reset Password")
+                        }
+                        .foregroundColor(Theme.Color.buttonTextAlt)
+                        .padding(.top, 20)
+                    }
+                    
+
+                    let modeButtonTitle = isRegisterPresented ? "Already have an account? Sign in" : "Create a new account"
+                    Button(modeButtonTitle) {
+                        isRegisterPresented.toggle()
                     }
                     .foregroundColor(Theme.Color.buttonTextAlt)
                     .padding(.top, 20)
+                    
+                    Spacer()
+                    
                 }
                 
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "xmark")
+                            .foregroundColor(Theme.Color.buttonTextAlt)
+                    }
 
-                let modeButtonTitle = isRegisterPresented ? "Already have an account? Sign in" : "Create a new account"
-                Button(modeButtonTitle) {
-                    isRegisterPresented.toggle()
                 }
-                .foregroundColor(Theme.Color.buttonTextAlt)
-                .padding(.top, 20)
-                
-                Spacer()
-                
             }
             
         }
-        
         
     }
 }
