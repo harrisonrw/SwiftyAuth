5e784719abf284dc3860bd5a9ec4a76fd48ab052
Robert Harrison <harrisonrw@gmail.com>
2021-08-09 11:32:18 -0700

    Embed authentication fields in a Scroll View

--- SwiftyAuth/SwiftyAuth/AuthenticationView.swift
+++ SwiftyAuth/SwiftyAuth/AuthenticationView.swift
@@ -30,107 +30,112 @@ struct AuthenticationView: View {
                 Theme.Color.background
                     .edgesIgnoringSafeArea(.all)
                 
-                VStack {
+                ScrollView(.vertical) {
                     
-                    Image("swift-logo-orange")
-                        .padding()
-                    
-                    if isRegisterPresented {
+                    VStack {
                         
-                        TextField("First Name", text: $firstName)
-                            .frame(height: 54)
-                            .textFieldStyle(.plain)
-                            .textContentType(.givenName)
-                            .padding(.horizontal, 10)
-                            .cornerRadius(8)
-                            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
-                            .padding(.horizontal, 20)
-                            .padding(.vertical, 4)
+                        Image("swift-logo-orange")
+                            .padding()
                         
-                        TextField("Last Name", text: $lastName)
-                            .frame(height: 54)
-                            .textFieldStyle(.plain)
-                            .textContentType(.familyName)
-                            .padding(.horizontal, 10)
-                            .cornerRadius(8)
-                            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
-                            .padding(.horizontal, 20)
-                            .padding(.vertical, 4)
-                        
-                    }
-                    
-                    TextField("Email", text: $email)
-                        .frame(height: 54)
-                        .textFieldStyle(.plain)
-                        .textContentType(.emailAddress)
-                        .padding(.horizontal, 10)
-                        .cornerRadius(8)
-                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 4)
-                    
-                    
-                    SecureField("Password", text: $password)
-                        .frame(height: 54)
-                        .textFieldStyle(.plain)
-                        .textContentType(.password)
-                        .padding(.horizontal, 10)
-                        .cornerRadius(8)
-                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 4)
-                    
-                    Button {
-                        
-                        guard isInputValid() else { return }
-                        
-                        Task {
-                            do {
-                                
-                                if isRegisterPresented {
-                                    let _ = try await UserManager.shared.register(firstName: firstName, lastName: lastName, email: email, password: password)
-                                } else {
-                                    let _ = try await UserManager.shared.signIn(email: email, password: password)
-                                }
-                                presentationMode.wrappedValue.dismiss()
-                                
-                            } catch {
-                                print(error.localizedDescription)
-                            }
+                        if isRegisterPresented {
+                            
+                            TextField("First Name", text: $firstName)
+                                .frame(height: 54)
+                                .textFieldStyle(.plain)
+                                .textContentType(.givenName)
+                                .padding(.horizontal, 10)
+                                .cornerRadius(8)
+                                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
+                                .padding(.horizontal, 20)
+                                .padding(.vertical, 4)
+                            
+                            TextField("Last Name", text: $lastName)
+                                .frame(height: 54)
+                                .textFieldStyle(.plain)
+                                .textContentType(.familyName)
+                                .padding(.horizontal, 10)
+                                .cornerRadius(8)
+                                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
+                                .padding(.horizontal, 20)
+                                .padding(.vertical, 4)
+                            
                         }
                         
-                    } label: {
-                        let actionButtonTitle = isRegisterPresented ? "Register" : "Sign In"
-                        Text(actionButtonTitle)
-                            .frame(minWidth: 0, maxWidth: .infinity)
-                            .padding()
-                            .foregroundColor(Theme.Color.buttonText)
-                            .background(Theme.Color.buttonBackground)
-                            .cornerRadius(Theme.cornerRadius)
-                            .font(.system(size: 18.0, weight: .semibold))
-                            .padding(.horizontal, 20.0)
+                        TextField("Email", text: $email)
+                            .frame(height: 54)
+                            .textFieldStyle(.plain)
+                            .textContentType(.emailAddress)
+                            .padding(.horizontal, 10)
+                            .cornerRadius(8)
+                            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
+                            .padding(.horizontal, 20)
                             .padding(.vertical, 4)
-                    }
-                    
-                    if !isRegisterPresented {
-                        Button("Reset Password") {
-                            print("Reset Password")
+                        
+                        
+                        SecureField("Password", text: $password)
+                            .frame(height: 54)
+                            .textFieldStyle(.plain)
+                            .textContentType(.password)
+                            .padding(.horizontal, 10)
+                            .cornerRadius(8)
+                            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5))
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 4)
+                        
+                        Button {
+                            
+                            guard isInputValid() else { return }
+                            
+                            Task {
+                                do {
+                                    
+                                    if isRegisterPresented {
+                                        let _ = try await UserManager.shared.register(firstName: firstName, lastName: lastName, email: email, password: password)
+                                    } else {
+                                        let _ = try await UserManager.shared.signIn(email: email, password: password)
+                                    }
+                                    presentationMode.wrappedValue.dismiss()
+                                    
+                                } catch {
+                                    print(error.localizedDescription)
+                                }
+                            }
+                            
+                        } label: {
+                            let actionButtonTitle = isRegisterPresented ? "Register" : "Sign In"
+                            Text(actionButtonTitle)
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .padding()
+                                .foregroundColor(Theme.Color.buttonText)
+                                .background(Theme.Color.buttonBackground)
+                                .cornerRadius(Theme.cornerRadius)
+                                .font(.system(size: 18.0, weight: .semibold))
+                                .padding(.horizontal, 20.0)
+                                .padding(.vertical, 4)
+                        }
+                        
+                        if !isRegisterPresented {
+                            Button("Reset Password") {
+                                print("Reset Password")
+                            }
+                            .foregroundColor(Theme.Color.buttonTextAlt)
+                            .padding(.top, 20)
+                        }
+                        
+
+                        let modeButtonTitle = isRegisterPresented ? "Already have an account? Sign in" : "Create a new account"
+                        Button(modeButtonTitle) {
+                            isRegisterPresented.toggle()
                         }
                         .foregroundColor(Theme.Color.buttonTextAlt)
                         .padding(.top, 20)
+                        
+                        Spacer()
+                        
                     }
-                    
-
-                    let modeButtonTitle = isRegisterPresented ? "Already have an account? Sign in" : "Create a new account"
-                    Button(modeButtonTitle) {
-                        isRegisterPresented.toggle()
-                    }
-                    .foregroundColor(Theme.Color.buttonTextAlt)
-                    .padding(.top, 20)
-                    
-                    Spacer()
                     
                 }
+                .frame(maxWidth: .infinity)
                 
             }
             .toolbar {
